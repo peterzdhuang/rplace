bafa32d72312759edefdb5498e3e49eaaaecb922
Dricmoy Bhattacharjee <dricbhattacharjee@gmail.com>
2025-04-10 23:12:29 -0600

    canvas component added, with zoom resize and other mouse event handling

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,6 +1,19 @@
 <script setup lang="ts">
+import Canvas from '@/components/Canvas.vue'
 </script>
 
 <template>
-    
+  <div class="home-view">
+    <Canvas />
+  </div>
 </template>
+
+
+<style scoped>
+.home-view {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+</style>
