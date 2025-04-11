7bc12c7ffd9cb8084f96377bdfb869f2fe84b0bf
peterzdhuang <peterzdhuang@gmail.com>
2025-04-10 21:53:49 -0600

    hub implementation

--- backend/server/websocket.go
+++ backend/server/websocket.go
@@ -8,6 +8,38 @@ import (
 	"github.com/google/uuid"
 )
 
+
+func (h *Hub) Run() {
+	for {
+		select {
+		case client := <- hub.register:
+			h.mu.Lock()
+			h.clients[client.uuid] = client
+			h.mu.Unlock()
+			log.Printf("Client connected: %s (%s)", client.Username, client.uuid)
+		case client := <-h.unregister:
+			h.mu.Lock()
+			if _, ok := h.clients[client.uuid]; ok {
+				delete(h.clients, client.uuid)
+				close(client.Send)
+				log.Printf("Client disconnected: %s (%s)", client.Username, client.uuid)
+			}
+			h.mu.Unlock()
+		case message := <- h.broadcast:
+			h.mu.Lock()
+			for _, client := range h.clients {
+				select {
+				case client.Send <- message:
+				default:
+					close(client.Send)
+					delete(h.clients, client.uuid)
+				}
+			}
+			h.mu.Unlock()
+		}
+		
+	}
+}
 func InitWebSocket() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
@@ -36,9 +68,11 @@ func InitWebSocket() gin.HandlerFunc {
 			Type:   "init",
 			Pixels: board.Pixels,
 		}
-		initialState := json.Marshal(boardState)
+		initialState, _ := json.Marshal(boardState)
 		board.mu.RUnlock()
 
-		client <- initialState
+		client.Send <- initialState
+
+		go client.
 	}
 }
